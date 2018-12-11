225b7aa11f409b874938d894addb8d802669a65f
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-12-11 22:47:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+#require "pry"
+def my_collect(array)
+firstarray = []
+array.each do |x|
+firstarray.push yield x
 
+end
+firstarray
+#binding.pry
+end
 
+#my_collect(languages) do |language|
+#  language.upcase
+#end
